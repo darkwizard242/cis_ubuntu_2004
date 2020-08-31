1c4c5fa1b3aacccf252e15c8b7236493c40c1469
darkwizard242 <am900820@gmail.com>
2020-08-31 17:59:49 -0400

    testinfra test cases for section 4 - added 4.1.1.1

---
+++ molecule/default/tests/test_section_04.py
@@ -0,0 +1,14 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_4_1_1_1_auditd_package(host):
+    assert host.package('auditd').is_installed
+
+
+def test_4_1_1_1_audispd_plugins_package(host):
+    assert host.package('audispd-plugins').is_installed

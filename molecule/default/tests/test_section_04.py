0d95487c2c7d646733aaab60c5747df86638be21
darkwizard242 <am900820@gmail.com>
2020-09-02 13:09:32 -0400

    testinfra functions for 4.1.13

--- molecule/default/tests/test_section_04.py
+++ molecule/default/tests/test_section_04.py
@@ -96,3 +96,11 @@ def test_4_1_12_audit_rule_file_exists(host):
 
 def test_4_1_12_audit_rule_file_isfile(host):
     assert host.file('/etc/audit/rules.d/4.1.12.rules').is_file
+
+
+def test_4_1_13_audit_rule_file_exists(host):
+    assert host.file('/etc/audit/rules.d/4.1.13.rules').exists
+
+
+def test_4_1_13_audit_rule_file_isfile(host):
+    assert host.file('/etc/audit/rules.d/4.1.13.rules').is_file

aa1e78c63f52fd9c68e271a6d79edb8e28b68469
darkwizard242 <am900820@gmail.com>
2020-09-02 14:30:06 -0400

    change assertion for file type testinfra functions

--- molecule/default/tests/test_section_04.py
+++ molecule/default/tests/test_section_04.py
@@ -19,123 +19,123 @@ def test_4_1_1_2_auditd_service(host):
 
 
 def test_4_1_3_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.3.rules').exists
+    host.file('/etc/audit/rules.d/4.1.3.rules').exists
 
 
 def test_4_1_3_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.3.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.3.rules').is_file
 
 
 def test_4_1_4_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.4.rules').exists
+    host.file('/etc/audit/rules.d/4.1.4.rules').exists
 
 
 def test_4_1_4_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.4.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.4.rules').is_file
 
 
 def test_4_1_5_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.5.rules').exists
+    host.file('/etc/audit/rules.d/4.1.5.rules').exists
 
 
 def test_4_1_5_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.5.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.5.rules').is_file
 
 
 def test_4_1_6_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.6.rules').exists
+    host.file('/etc/audit/rules.d/4.1.6.rules').exists
 
 
 def test_4_1_6_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.6.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.6.rules').is_file
 
 
 def test_4_1_7_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.7.rules').exists
+    host.file('/etc/audit/rules.d/4.1.7.rules').exists
 
 
 def test_4_1_7_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.7.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.7.rules').is_file
 
 
 def test_4_1_8_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.8.rules').exists
+    host.file('/etc/audit/rules.d/4.1.8.rules').exists
 
 
 def test_4_1_8_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.8.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.8.rules').is_file
 
 
 def test_4_1_9_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.9.rules').exists
+    host.file('/etc/audit/rules.d/4.1.9.rules').exists
 
 
 def test_4_1_9_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.9.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.9.rules').is_file
 
 
 def test_4_1_10_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.10.rules').exists
+    host.file('/etc/audit/rules.d/4.1.10.rules').exists
 
 
 def test_4_1_10_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.10.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.10.rules').is_file
 
 
 def test_4_1_11_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.11.rules').exists
+    host.file('/etc/audit/rules.d/4.1.11.rules').exists
 
 
 def test_4_1_11_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.11.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.11.rules').is_file
 
 
 def test_4_1_12_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.12.rules').exists
+    host.file('/etc/audit/rules.d/4.1.12.rules').exists
 
 
 def test_4_1_12_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.12.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.12.rules').is_file
 
 
 def test_4_1_13_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.13.rules').exists
+    host.file('/etc/audit/rules.d/4.1.13.rules').exists
 
 
 def test_4_1_13_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.13.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.13.rules').is_file
 
 
 def test_4_1_14_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.14.rules').exists
+    host.file('/etc/audit/rules.d/4.1.14.rules').exists
 
 
 def test_4_1_14_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.14.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.14.rules').is_file
 
 
 def test_4_1_15_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.15.rules').exists
+    host.file('/etc/audit/rules.d/4.1.15.rules').exists
 
 
 def test_4_1_15_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.15.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.15.rules').is_file
 
 
 def test_4_1_16_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/4.1.16.rules').exists
+    host.file('/etc/audit/rules.d/4.1.16.rules').exists
 
 
 def test_4_1_16_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/4.1.16.rules').is_file
+    host.file('/etc/audit/rules.d/4.1.16.rules').is_file
 
 
 def test_4_1_17_audit_rule_file_exists(host):
-    assert host.file('/etc/audit/rules.d/99_4.1.17.rules').exists
+    host.file('/etc/audit/rules.d/99_4.1.17.rules').exists
 
 
 def test_4_1_17_audit_rule_file_isfile(host):
-    assert host.file('/etc/audit/rules.d/99_4.1.17.rules').is_file
+    host.file('/etc/audit/rules.d/99_4.1.17.rules').is_file
 
 
 def test_4_2_1_1_rsyslog_package(host):

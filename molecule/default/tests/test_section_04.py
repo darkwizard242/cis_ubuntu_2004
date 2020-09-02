e9c602cfcde5f471590862ae8495f8472e013d43
darkwizard242 <am900820@gmail.com>
2020-09-02 13:05:54 -0400

    testinfra functions for 4.1.3

--- molecule/default/tests/test_section_04.py
+++ molecule/default/tests/test_section_04.py
@@ -16,3 +16,11 @@ def test_4_1_1_1_audispd_plugins_package(host):
 
 def test_4_1_1_2_auditd_service(host):
     assert host.service('auditd').is_enabled
+
+
+def test_4_1_3_audit_rule_file_exists(host):
+    assert host.file('/etc/audit/rules.d/4.1.3.rules').exists
+
+
+def test_4_1_3_audit_rule_file_isfile(host):
+    assert host.file('/etc/audit/rules.d/4.1.3.rules').is_file

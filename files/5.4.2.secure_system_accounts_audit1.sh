dae5463cdc099d8e5cead81af5bb1a69df607828
darkwizard242 <am900820@gmail.com>
2020-09-12 23:42:35 -0400

    5.4.2 audit 1 | utilize script instead of shell for ansible linting compliance

---
+++ files/5.4.2.secure_system_accounts_audit1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+set -eou pipefail
+
+awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $7!="'"$(which nologin)"'" && $7!="/bin/false") {print}' /etc/passwd

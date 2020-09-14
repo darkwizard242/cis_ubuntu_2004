fbb76b0888a7cdec49012dddf1d38365c25c73a1
darkwizard242 <am900820@gmail.com>
2020-09-14 17:08:59 -0400

    script for 6.2.13 | ensure no duplicate UIDs exist

---
+++ files/6.2.13.no_duplicate_uids.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+cut -f3 -d":" /etc/passwd | sort -n | uniq -c | while read x ;
+do
+  [ -z "$x" ] && break
+  set - $x
+  if [ $1 -gt 1 ];
+  then
+    users=$(awk -F: '($3 == n) { print $1 }' n=$2 /etc/passwd | xargs)
+    echo "Duplicate UID ($2): $users"
+  fi
+done

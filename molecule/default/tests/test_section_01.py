41c4e9457712a100e40ddb1185b3c25612a9dfec
darkwizard242 <am900820@gmail.com>
2020-09-15 18:37:14 -0400

    refactor testinfra functions for 1.1.1.3

--- molecule/default/tests/test_section_01.py
+++ molecule/default/tests/test_section_01.py
@@ -8,6 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 CRAMFS_MOD_FILE = "/etc/modprobe.d/1.1.1.1_cramfs.conf"
 FREEVXFS_MOD_FILE = "/etc/modprobe.d/1.1.1.2_freevxfs.conf"
+JFFS2_MOD_FILE = "/etc/modprobe.d/1.1.1.3_jffs2.conf"
 
 
 def test_1_1_1_1_file_exists(host):
@@ -91,23 +92,43 @@ def test_1_1_1_2_file_group(host):
 
 
 def test_1_1_1_3_file_exists(host):
-    assert host.file('/etc/modprobe.d/1.1.1.3_jffs2.conf').exists
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 1.1.1.3
+    Tests if /etc/modprobe.d/1.1.1.3_jffs2.conf file exists
+    """
+    assert host.file(JFFS2_MOD_FILE).exists
 
 
 def test_1_1_1_3_file_isfile(host):
-    assert host.file('/etc/modprobe.d/1.1.1.3_jffs2.conf').is_file
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 1.1.1.3
+    Tests if /etc/modprobe.d/1.1.1.3_jffs2.conf is a file
+    """
+    assert host.file(JFFS2_MOD_FILE).is_file
 
 
 def test_1_1_1_3_file_mode(host):
-    assert host.file('/etc/modprobe.d/1.1.1.3_jffs2.conf').mode == 0o644
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 1.1.1.3
+    Tests if /etc/modprobe.d/1.1.1.3_jffs2.conf has 0644 mode
+    """
+    assert host.file(JFFS2_MOD_FILE).mode == 0o644
 
 
 def test_1_1_1_3_file_user(host):
-    assert host.file('/etc/modprobe.d/1.1.1.3_jffs2.conf').user == 'root'
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 1.1.1.3
+    Tests if /etc/modprobe.d/1.1.1.3_jffs2.conf is owned by user root
+    """
+    assert host.file(JFFS2_MOD_FILE).user == 'root'
 
 
 def test_1_1_1_3_file_group(host):
-    assert host.file('/etc/modprobe.d/1.1.1.3_jffs2.conf').group == 'root'
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 1.1.1.3
+    Tests if /etc/modprobe.d/1.1.1.3_jffs2.conf is owned by group root
+    """
+    assert host.file(JFFS2_MOD_FILE).group == 'root'
 
 
 def test_1_1_1_4_file_exists(host):

94a40450b861f98c75ba159ba7c7b306fa80d0bc
darkwizard242 <am900820@gmail.com>
2020-09-16 19:54:47 -0400

    refactor testinfra function for 3.4.3 and 3.4.4

--- molecule/default/tests/test_section_03.py
+++ molecule/default/tests/test_section_03.py
@@ -8,6 +8,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 MOD_DCCP_FILE = "/etc/modprobe.d/3.4.1_dccp.conf"
 MOD_SCTP_FILE ="/etc/modprobe.d/3.4.2_sctp.conf"
+MOD_RDS_FILE = "/etc/modprobe.d/3.4.3_rds.conf"
+MOD_TIPC_FILE = "/etc/modprobe.d/3.4.4_tipc.conf"
 
 
 def test_3_4_1_file_exists(host):
@@ -91,43 +93,83 @@ def test_3_4_2_file_group(host):
 
 
 def test_3_4_3_file_exists(host):
-    assert host.file('/etc/modprobe.d/3.4.3_rds.conf').exists
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.3
+    Tests if /etc/modprobe.d/3.4.3_rds.conf file exists
+    """
+    assert host.file(MOD_RDS_FILE).exists
 
 
 def test_3_4_3_file_isfile(host):
-    assert host.file('/etc/modprobe.d/3.4.3_rds.conf').is_file
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.3
+    Tests if /etc/modprobe.d/3.4.3_rds.conf file is a file
+    """
+    assert host.file(MOD_RDS_FILE).is_file
 
 
 def test_3_4_3_file_mode(host):
-    assert host.file('/etc/modprobe.d/3.4.3_rds.conf').mode == 0o644
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.3
+    Tests if /etc/modprobe.d/3.4.3_rds.conf file has mode 0644
+    """
+    assert host.file(MOD_RDS_FILE).mode == 0o644
 
 
 def test_3_4_3_file_user(host):
-    assert host.file('/etc/modprobe.d/3.4.3_rds.conf').user == 'root'
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.3
+    Tests if /etc/modprobe.d/3.4.3_rds.conf file is owned by user root
+    """
+    assert host.file(MOD_RDS_FILE).user == 'root'
 
 
 def test_3_4_3_file_group(host):
-    assert host.file('/etc/modprobe.d/3.4.3_rds.conf').group == 'root'
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.3
+    Tests if /etc/modprobe.d/3.4.3_rds.conf file is owned by group root
+    """
+    assert host.file(MOD_RDS_FILE).group == 'root'
 
 
 def test_3_4_4_file_exists(host):
-    assert host.file('/etc/modprobe.d/3.4.4_tipc.conf').exists
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.4
+    Tests if /etc/modprobe.d/3.4.4_tipc.conf file exists
+    """
+    assert host.file(MOD_TIPC_FILE).exists
 
 
 def test_3_4_4_file_isfile(host):
-    assert host.file('/etc/modprobe.d/3.4.4_tipc.conf').is_file
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.4
+    Tests if /etc/modprobe.d/3.4.4_tipc.conf file is a file
+    """
+    assert host.file(MOD_TIPC_FILE).is_file
 
 
 def test_3_4_4_file_mode(host):
-    assert host.file('/etc/modprobe.d/3.4.4_tipc.conf').mode == 0o644
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.4
+    Tests if /etc/modprobe.d/3.4.4_tipc.conf file has mode 0644
+    """
+    assert host.file(MOD_TIPC_FILE).mode == 0o644
 
 
 def test_3_4_4_file_user(host):
-    assert host.file('/etc/modprobe.d/3.4.4_tipc.conf').user == 'root'
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.4
+    Tests if /etc/modprobe.d/3.4.4_tipc.conf file is owned by user root
+    """
+    assert host.file(MOD_TIPC_FILE).user == 'root'
 
 
 def test_3_4_4_file_group(host):
-    assert host.file('/etc/modprobe.d/3.4.4_tipc.conf').group == 'root'
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 3.4.4
+    Tests if /etc/modprobe.d/3.4.4_tipc.conf file is owned by group root
+    """
+    assert host.file(MOD_TIPC_FILE).group == 'root'
 
 
 def test_3_5_1_1_ufw_package(host):

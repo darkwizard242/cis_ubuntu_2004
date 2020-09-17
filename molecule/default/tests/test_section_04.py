a1be28407504962f73f292f03f39a898bea12ad7
darkwizard242 <am900820@gmail.com>
2020-09-16 20:29:36 -0400

    refactor testinfra function for 4.1.7

--- molecule/default/tests/test_section_04.py
+++ molecule/default/tests/test_section_04.py
@@ -10,6 +10,17 @@ RULE_FILE_413 = "/etc/audit/rules.d/4.1.3.rules"
 RULE_FILE_414 = "/etc/audit/rules.d/4.1.4.rules"
 RULE_FILE_415 = "/etc/audit/rules.d/4.1.5.rules"
 RULE_FILE_416 = "/etc/audit/rules.d/4.1.6.rules"
+RULE_FILE_417 = "/etc/audit/rules.d/4.1.7.rules"
+RULE_FILE_418 = "/etc/audit/rules.d/4.1.8.rules"
+RULE_FILE_419 = "/etc/audit/rules.d/4.1.9.rules"
+RULE_FILE_4110 = "/etc/audit/rules.d/4.1.10.rules"
+RULE_FILE_4111 = "/etc/audit/rules.d/4.1.11.rules"
+RULE_FILE_4112 = "/etc/audit/rules.d/4.1.12.rules"
+RULE_FILE_4113 = "/etc/audit/rules.d/4.1.13.rules"
+RULE_FILE_4114 = "/etc/audit/rules.d/4.1.14.rules"
+RULE_FILE_4115 = "/etc/audit/rules.d/4.1.15.rules"
+RULE_FILE_4116 = "/etc/audit/rules.d/4.1.16.rules"
+RULE_FILE_4117 = "/etc/audit/rules.d/4.1.17.rules"
 
 
 def test_4_1_1_1_auditd_package(host):
@@ -89,11 +100,19 @@ def test_4_1_6_audit_rule_file_isfile(host):
 
 
 def test_4_1_7_audit_rule_file_exists(host):
-    host.file('/etc/audit/rules.d/4.1.7.rules').exists
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 4.1.7
+    Tests if /etc/audit/rules.d/4.1.7.rules file exists
+    """
+    host.file(RULE_FILE_416).exists
 
 
 def test_4_1_7_audit_rule_file_isfile(host):
-    host.file('/etc/audit/rules.d/4.1.7.rules').is_file
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 4.1.7
+    Tests if /etc/audit/rules.d/4.1.7.rules file is a file
+    """
+    host.file(RULE_FILE_416).is_file
 
 
 def test_4_1_8_audit_rule_file_exists(host):

be81cd09778ae1df12f50e083ec6bc040da588cd
darkwizard242 <am900820@gmail.com>
2020-09-16 20:31:26 -0400

    refactor testinfra function for 4.1.10

--- molecule/default/tests/test_section_04.py
+++ molecule/default/tests/test_section_04.py
@@ -148,11 +148,19 @@ def test_4_1_9_audit_rule_file_isfile(host):
 
 
 def test_4_1_10_audit_rule_file_exists(host):
-    host.file('/etc/audit/rules.d/4.1.10.rules').exists
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 4.1.10
+    Tests if /etc/audit/rules.d/4.1.10.rules file exists
+    """
+    host.file(RULE_FILE_4110).exists
 
 
 def test_4_1_10_audit_rule_file_isfile(host):
-    host.file('/etc/audit/rules.d/4.1.10.rules').is_file
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 4.1.10
+    Tests if /etc/audit/rules.d/4.1.10.rules file is a file
+    """
+    host.file(RULE_FILE_4110).is_file
 
 
 def test_4_1_11_audit_rule_file_exists(host):

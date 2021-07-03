49059be16a4009807bccae4a8f62969698894da1
darkwizard242 <am900820@gmail.com>
2021-07-02 22:56:01 -0400

    update py test for 6.1.3

--- molecule/default/tests/test_section_06.py
+++ molecule/default/tests/test_section_06.py
@@ -97,6 +97,46 @@ def test_6_1_2_etc_passwd_group(host):
     assert host.file(ETC_PASSWD).group == 'root'
 
 
+def test_6_1_3_etc_group_exists(host):
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.3
+    Tests if /etc/passwd- file exists
+    """
+    assert host.file(ETC_PASSWD_DASH).exists
+
+
+def test_6_1_3_etc_group_isfile(host):
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.3
+    Tests if /etc/passwd- is a file
+    """
+    assert host.file(ETC_PASSWD_DASH).is_file
+
+
+def test_6_1_3_etc_group_mode(host):
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.3
+    Tests if /etc/passwd- has 0644 mode
+    """
+    assert host.file(ETC_PASSWD_DASH).mode == 0o644
+
+
+def test_6_1_3_etc_group_user(host):
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.3
+    Tests if /etc/passwd- is owned by user root
+    """
+    assert host.file(ETC_PASSWD_DASH).user == 'root'
+
+
+def test_6_1_3_etc_group_group(host):
+    """
+    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.3
+    Tests if /etc/passwd- is owned by group root
+    """
+    assert host.file(ETC_PASSWD_DASH).group == 'root'
+
+
 def test_6_1_4_etc_group_exists(host):
     """
     CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.4
@@ -175,46 +215,6 @@ def test_6_1_6_etc_shadow_group(host):
     Tests if /etc/shadow is owned by group root
     """
     assert host.file(ETC_SHADOW).group == 'root'
-
-
-def test_6_1_6_etc_group_exists(host):
-    """
-    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.6
-    Tests if /etc/passwd- file exists
-    """
-    assert host.file(ETC_PASSWD_DASH).exists
-
-
-def test_6_1_6_etc_group_isfile(host):
-    """
-    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.6
-    Tests if /etc/passwd- is a file
-    """
-    assert host.file(ETC_PASSWD_DASH).is_file
-
-
-def test_6_1_6_etc_group_mode(host):
-    """
-    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.6
-    Tests if /etc/passwd- has 0644 mode
-    """
-    assert host.file(ETC_PASSWD_DASH).mode == 0o644
-
-
-def test_6_1_6_etc_group_user(host):
-    """
-    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.6
-    Tests if /etc/passwd- is owned by user root
-    """
-    assert host.file(ETC_PASSWD_DASH).user == 'root'
-
-
-def test_6_1_6_etc_group_group(host):
-    """
-    CIS Ubuntu 20.04 v1.0.0 - Rule # 6.1.6
-    Tests if /etc/passwd- is owned by group root
-    """
-    assert host.file(ETC_PASSWD_DASH).group == 'root'
 
 
 def test_6_1_7_etc_shadow_dash_exists(host):
